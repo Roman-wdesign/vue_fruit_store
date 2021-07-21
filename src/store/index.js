cb608facb5f824fc162b41c993ac6b38d6cdf1c0
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-07-21 14:03:16 +0700

    axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import actions from './actions/actions';
+import getters from './getters/getters';
+import mutations from './mutations/mutations';
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+Vue.use(Vuex);
+
+let store= new Vuex.Store({
+  state:{},
+  mutations,
+
+  actions,
+
+  getters
 })
+
+export default store
