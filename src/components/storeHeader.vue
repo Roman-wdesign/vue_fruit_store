8478d565a0d5f6a76c343123ba3cd9370b681e7d
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-07-29 14:54:24 +0700

    header

--- src/components/storeHeader.vue
+++ src/components/storeHeader.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="header">
-  <h3>Header</h3>
-</div>
+  <div class="header">
+    <div class="header__picture">
+      <v-img
+          class="header__picture-img"
+          src="fruit_logo.png"
+      ></v-img>
+    </div>
+
+    <h3 class="header-header">Header</h3>
+  </div>
 </template>
 
 <script>
