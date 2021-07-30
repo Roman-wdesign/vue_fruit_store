5eec9d8c995686b91ee024477e180ddce5d4a6b7
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-07-30 09:07:32 +0700

    header/2

--- src/components/storeHeader.vue
+++ src/components/storeHeader.vue
@@ -1,19 +1,169 @@
 <template>
   <div class="header">
-    <div class="header__picture">
-      <v-img
-          class="header__picture-img"
-          src="fruit_logo.png"
-      ></v-img>
+    <div class="header__one">
+      <div class="header__one__picture">
+        <v-img
+            class="header__one__picture-img"
+            src="fruit_logo.png"
+        ></v-img>
+      </div>
+      <div class="header__one__search">
+        <v-toolbar
+            dark
+            color="teal"
+        >
+          <v-toolbar-title>State selection</v-toolbar-title>
+          <v-autocomplete
+              v-model="select"
+              :loading="loading"
+              :items="items"
+              :search-input.sync="search"
+              cache-items
+              class="mx-4"
+              flat
+              hide-no-data
+              hide-details
+              label="What fruit do you need?"
+              solo-inverted
+          ></v-autocomplete>
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </div>
     </div>
+    <div class="header__one__list">
+      <div class="text-center">
+        <v-menu
+            open-on-hover
+            bottom
+            offset-y
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                color="primary"
+                dark
+                v-bind="attrs"
+                v-on="on"
+            >
+              <div class="header__two-icons"><i class="fas fa-list-ul"></i></div>
+              All Categories
+            </v-btn>
+          </template>
 
-    <h3 class="header-header">Header</h3>
+          <v-list>
+            <v-list-item
+                v-for="(click, index) in clicks"
+                :key="index"
+            >
+              <v-list-item-title>{{ click.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </div>
+    <div class="header__two">
+      <div class="header__two-icons">
+        <i class="fas fa-sign-in-alt"></i>
+        <i class="fas fa-shopping-bag"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "storeHeader"
+  name: "storeHeader",
+  data: () => ({
+    loading: false,
+    items: [],
+    search: null,
+    select: null,
+    states: [
+      'Alabama',
+      'Alaska',
+      'American Samoa',
+      'Arizona',
+      'Arkansas',
+      'California',
+      'Colorado',
+      'Connecticut',
+      'Delaware',
+      'District of Columbia',
+      'Federated States of Micronesia',
+      'Florida',
+      'Georgia',
+      'Guam',
+      'Hawaii',
+      'Idaho',
+      'Illinois',
+      'Indiana',
+      'Iowa',
+      'Kansas',
+      'Kentucky',
+      'Louisiana',
+      'Maine',
+      'Marshall Islands',
+      'Maryland',
+      'Massachusetts',
+      'Michigan',
+      'Minnesota',
+      'Mississippi',
+      'Missouri',
+      'Montana',
+      'Nebraska',
+      'Nevada',
+      'New Hampshire',
+      'New Jersey',
+      'New Mexico',
+      'New York',
+      'North Carolina',
+      'North Dakota',
+      'Northern Mariana Islands',
+      'Ohio',
+      'Oklahoma',
+      'Oregon',
+      'Palau',
+      'Pennsylvania',
+      'Puerto Rico',
+      'Rhode Island',
+      'South Carolina',
+      'South Dakota',
+      'Tennessee',
+      'Texas',
+      'Utah',
+      'Vermont',
+      'Virgin Island',
+      'Virginia',
+      'Washington',
+      'West Virginia',
+      'Wisconsin',
+      'Wyoming',
+    ],
+    clicks: [
+      {title: 'Click Me'},
+      {title: 'Click Me'},
+      {title: 'Click Me'},
+      {title: 'Click Me 2'},
+    ],
+  }),
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val)
+    },
+  },
+  methods: {
+    querySelections(v) {
+      this.loading = true
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.states.filter(e => {
+          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+        })
+        this.loading = false
+      }, 500)
+    },
+  },
 }
 </script>
 
