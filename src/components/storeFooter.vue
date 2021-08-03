e2de3444883d60f081ef54985e7711a0da1324ca
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-08-03 15:46:27 +0700

    footer

--- src/components/storeFooter.vue
+++ src/components/storeFooter.vue
@@ -1,33 +1,40 @@
 <template>
-<div class="footer">
-  <h3>Footer</h3>
-  <div class="header__one__picture">
-    <v-img
-        class="header__one__picture-img"
-        src="fruit_logo.png"
-    ></v-img>
-    <div class="footer__links">
-      <div class="footer__social">
-        <a href="https://twitter.com/twitter"><i class="fab fa-twitter"></i></a>
-        <a href=" https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-        <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-        <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+  <div class="footer">
+    <h3>Footer</h3>
+    <div class="footer__first">
+      <div class="header__one__picture">
+        <v-img
+            class="header__one__picture-img"
+            src="fruit_logo.png"
+        ></v-img>
       </div>
-      <div class="footer__payment">
-        <a href="https://www.visa.com.ru/"><i class="fab fa-cc-visa" ></i></a>
-        <a href="https://www.mastercard.us/en-us.html"><i class="fab fa-cc-mastercard"></i></a>
+      <div class="footer__links">
+        <div class="footer__links-banner"><p>Follow Us</p></div>
+        <div class="footer__links__social">
+
+          <a href="https://twitter.com/twitter"><i class="fab fa-twitter"></i></a>
+          <a href=" https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+        </div>
+        <div class="footer__links__payment">
+          <a href="https://www.visa.com.ru/"><i class="fab fa-cc-visa"></i></a>
+          <a href="https://www.mastercard.us/en-us.html"><i class="fab fa-cc-mastercard"></i></a>
+        </div>
       </div>
     </div>
-  </div>
+    <div class="footer__second">
+      <a href="tel:Phone: ORANGE(880000000)">tel:Phone: ORANGE(880000000)</a>
+      <span>Email: mail@wbcplace.ru</span>
+    </div>
+    <div class="catalog__divider">
+      <v-divider></v-divider>
+    </div>
+    <div class="footer-all_rights">
+      <p>Copyright © 2021 Orange. All rights reserved.</p>
+    </div>
 
-  <div class="catalog__divider">
-    <v-divider></v-divider>
   </div>
-  <div class="footer-all_rights">
-    <p>Copyright © 2021 Orange. All rights reserved.</p>
-  </div>
-
-</div>
 </template>
 
 <script>
