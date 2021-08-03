beff60ef6277e3134b20338668cf11ac2488df33
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-08-03 11:18:14 +0700

    correct social links icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
 import './styles/style.scss'
+import vuetify from './plugins/vuetify'
+
 Vue.config.productionTip = false
 
 new Vue({
