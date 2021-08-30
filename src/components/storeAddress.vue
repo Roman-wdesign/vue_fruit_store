34e1bfe0f2a0a270a4958ec5b3eaf0c6c01722c3
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-08-30 14:58:45 +0700

    Yandex map addresses list 2

--- src/components/storeAddress.vue
+++ src/components/storeAddress.vue
@@ -13,14 +13,16 @@
         </div>
       </div>
       <h3>Our addresses</h3>
-      <div id="yandex-map" v-show="checkbox" style="width: 100%; height: 500px; padding: 2rem 0"></div>
+      <div id="yandex-map" v-show="checkbox" style="width: 100%; height: 500px; padding: 2rem 0"
+           :groups=groups
+      ></div>
 
       <div class="yandex_map_results" v-show="!checkbox">
         <ul>
           <li
-              v-for="value in groups"
-              :key="value">
-            {{ value.one }}
+              v-for="(value, index) in groups"
+              :key="index">
+            {{ value }}
           </li>
         </ul>
       </div>
@@ -38,37 +40,35 @@ export default {
     groups: [],
   }),
 
-  mounted() {
-    let scriptYandexMap = document.createElement('script');
-    scriptYandexMap.setAttribute('src', 'https://api-maps.yandex.ru/2.1/?apikey=0c94976f-5b19-4562-adab-814369f61605&lang=ru_RU');
-    document.head.appendChild(scriptYandexMap);
-    scriptYandexMap.addEventListener("load", this.initializeYandexMap);
 
-
-  },
   methods: {
-    initializeYandexMap() {
-      // eslint-disable-next-line no-undef
-      ymaps.ready(() => {
-        // eslint-disable-next-line no-undef
-        this.map = new ymaps.Map("yandex-map", {
-          center: [55.778922068976605, 37.71569549999996],
-          zoom: 15,
-          controls: ['fullscreenControl', 'rulerControl'],
-          searchControlProvider: 'yandex#search'
-        });
-        // eslint-disable-next-line no-undef
-        let searchControl = new ymaps.control.SearchControl({
-          options: {
-            provider: 'yandex#search'
-          }
-        });
-        this.map.controls.add(searchControl);
-        searchControl.search('Фрукты и овощи');
 
-        this.getCoordData().then(() => this.setMarkers());
-      });
+    initializeYandexMap() {
+      return new Promise(r =>
+          // eslint-disable-next-line no-undef
+          ymaps.ready(() => {
+            // eslint-disable-next-line no-undef
+            this.map = new ymaps.Map("yandex-map", {
+              center: [55.778922068976605, 37.71569549999996],
+              zoom: 10,
+              controls: ['fullscreenControl', 'rulerControl'],
+              searchControlProvider: 'yandex#search'
+            });
+
+            // eslint-disable-next-line no-undef
+            let searchControl = new ymaps.control.SearchControl({
+              options: {
+                provider: 'yandex#search'
+              }
+
+            });
+
+            this.map.controls.add(searchControl);
+            searchControl.search('Фрукты и овощи');
+            r();
+          }));
     },
+
 
     toggleCheckbox() {
       this.checkbox = !this.checkbox
@@ -77,23 +77,33 @@ export default {
     getCoordData() {
       return new Promise(r => setTimeout(() => {
         this.groups = [
-          {
-            one: "Москва, Бакунинская улица, 32/36к1", two: [55.77536806896744,37.68623949999987]
-          },
-          {
-            one: "Москва,Плетешковский переулок, 3с2", two: [55.77161106895781,37.67528899999994]
-          },
-          {
-            one: "Москва, Щербаковская улица, 35", two: [55.782870568956966,37.729619499999956]
-          },
-          {
-            one: "Москва, улица Земляной Вал, 64с2 ", two: [55.74618756898188,37.654708499999956]
-          },
+          [55.77536806896744, 37.68623949999987, "Москва, Бакунинская улица, 32/36к1"],
+          [55.77161106895781, 37.67528899999994, "Москва,Плетешковский переулок, 3с2"],
+          [55.782870568956966, 37.729619499999956, "Москва, Щербаковская улица, 35"],
+          [55.74618756898188, 37.654708499999956, "Москва, улица Земляной Вал, 64с2"],
         ];
         r();
       }, 1000));
     },
-  }
+    setMarkers() {
+      for (let i = 0; i < this.groups.length; i++) {
+        // eslint-disable-next-line no-undef
+        let placemark = new ymaps.Placemark(this.groups[i]);
+        this.map.geoObjects.add(placemark);
+      }
+    },
+  },
+  mounted() {
+    let scriptYandexMap = document.createElement('script');
+    scriptYandexMap.setAttribute('src', 'https://api-maps.yandex.ru/2.1/?apikey=0c94976f-5b19-4562-adab-814369f61605&lang=ru_RU');
+    document.head.appendChild(scriptYandexMap);
+
+    scriptYandexMap.addEventListener('load', () => Promise.all([
+      this.initializeYandexMap(),
+      this.getCoordData(),
+
+    ]).then(this.setMarkers));
+  },
 }
 </script>
 
